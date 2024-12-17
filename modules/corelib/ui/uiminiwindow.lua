0d820286fe1e915072a2c88dbe9a79672f8f4e22
Victor <vasconcellosdevictor@gmail.com>
2024-12-17 00:50:03 -0300

    GUI +

--- modules/corelib/ui/uiminiwindow.lua
+++ modules/corelib/ui/uiminiwindow.lua
@@ -244,7 +244,7 @@ function UIMiniWindow:onDragLeave(droppedWidget, mousePos)
 
     self:saveParent(self:getParent())
 
-    -- Note: It seems to prevent the minimap, inventory, and health widgets from moving off the interface panel.
+    --[[ Note: It seems to prevent the minimap, inventory, and health widgets from moving off the interface panel.
     if self.moveOnlyToMain or droppedWidget and droppedWidget.onlyPhantomDrop then
         if not (droppedWidget) or (self.moveOnlyToMain and not (droppedWidget.onlyPhantomDrop)) or
             (not (self.moveOnlyToMain) and droppedWidget.onlyPhantomDrop) then
@@ -253,7 +253,7 @@ function UIMiniWindow:onDragLeave(droppedWidget, mousePos)
             self.oldParentDrag:insertChild(self.oldParentDragIndex, self)
             self.movedWidget = nil
         end
-    end
+    end]]
 end
 
 function UIMiniWindow:onDragMove(mousePos, mouseMoved)
